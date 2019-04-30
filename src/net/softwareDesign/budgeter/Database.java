2260e152a241ebe79284d688091ae103e76fadc6
Peter Geertsema <peterjgeertsema@gmail.com>
2019-04-30 14:27:45 -0400

    Work on the backend

---
+++ src/net/softwareDesign/budgeter/Database.java
@@ -0,0 +1,21 @@
+package net.softwareDesign.budgeter;
+
+public class Database {
+    /**
+     * Monthly income, in dollars
+     */
+    private int monthlyIncome;
+    /**
+     * Age of the user, in years
+     */
+    private double age;
+    /**
+     * User's current retirement savings
+     */
+    private int retirementSavings;
+    /**
+     * User's current emergency savings
+     */
+    private int emergencySavings;
+
+}
