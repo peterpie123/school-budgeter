3ec14d03d6581750283f117a7c03ca8968810fb4
Peter Geertsema <peterjgeertsema@gmail.com>
2019-05-06 14:12:05 -0400

    Tethered database to GUI

--- src/net/softwareDesign/budgeter/Database.java
+++ src/net/softwareDesign/budgeter/Database.java
@@ -1,10 +1,19 @@
 package net.softwareDesign.budgeter;
 
+import java.util.Date;
+
 public class Database {
+
     /**
      * Monthly income, in dollars
      */
     private int monthlyIncome;
+
+    private int monthlyUtilities;
+
+    private int foodExpenses;
+
+    private Date retireDate;
     /**
      * Age of the user, in years
      */
@@ -18,4 +27,12 @@ public class Database {
      */
     private int emergencySavings;
 
+    public Database(int monthlyIncome, int monthlyUtilities,
+                    int foodExpenses, Date retireDate, int retirementSavings) {
+        this.monthlyIncome = monthlyIncome;
+        this.monthlyUtilities = monthlyUtilities;
+        this.foodExpenses = foodExpenses;
+        this.retireDate = retireDate;
+        this.retirementSavings = retirementSavings;
+    }
 }
